ddc9beaf2dc8a019891d8bc38185bfb506185b33
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-01-06 17:11:23 +0530

    used toJpeg to save the meme for the user

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -9,15 +9,16 @@ const Text = () => {
   return (
     <Draggable nodeRef={nodeRef}>
       {editMode ? (
-        <input ref={nodeRef}
-          onDoubleClick={() => setEditMode(false)}
+        <input
+          ref={nodeRef}
+          onDoubleClick={(e) => setEditMode(false)}
           value={val}
           onChange={(e) => setVal(e.target.value)}
         />
       ) : (
-        <h1 onDoubleClick={() => setEditMode(true)} ref={nodeRef}>
+        <p onDoubleClick={(e) => setEditMode(true)} ref={nodeRef}>
           {val}
-        </h1>
+        </p>
       )}
     </Draggable>
   );
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, createRef } from "react";
 import { Button } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import Text from "../components/Text";
+// import { exportComponentAsJPEG } from "react-component-export-image";
+import { toJpeg } from 'html-to-image';
 
 const EditPage = () => {
   const [params] = useSearchParams();
 
   const [count, setCount] = useState(0);
 
+  const memeRef = createRef();
+
   const addText = () => {
     setCount(count + 1);
   };
 
+  const handleExport = () => {
+    if (memeRef.current) {
+      toJpeg(memeRef.current, { quality: 0.95 })
+        .then((dataUrl) => {
+          const link = document.createElement("a");
+          link.href = dataUrl;
+          link.download = "meme.jpeg";
+          link.click();
+        })
+        .catch((error) => {
+          console.error("Error exporting image:", error);
+        });
+    }
+  };
+
   return (
     <div>
-      <div>
+      <div ref={memeRef} className="meme mt-5 mb-5">
         <img src={params.get("url")} alt="image-link" width={"250px"} />
         {Array(count)
           .fill(0)
@@ -23,7 +42,9 @@ const EditPage = () => {
           ))}
       </div>
       <Button onClick={addText}>Add Text</Button>
-      <Button variant="success" onClick={addText}>Save</Button>
+      <Button variant="success" onClick={handleExport}>
+        Save
+      </Button>
     </div>
   );
 };
